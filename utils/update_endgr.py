2c7c98f1619c16390099c1482a1e94e68d1085e2
Dakotah Hurda <99497837+dakotah-hurda@users.noreply.github.com>
2023-04-11 20:52:37 -0500

    Update update_endgr.py

--- utils/update_endgr.py
+++ utils/update_endgr.py
@@ -8,7 +8,7 @@ def update_endgr(endpoint_id: str, ise_api_creds: str, endgr_id: str):
     This function handles adding the provided list of new_macs to the provided endpoint group.
     """
 
-    url = f"https://cise520lpi3.milwaukeecountywi.gov/ers/config/endpoint/{endpoint_id}"
+    url = f"https://ISE-URL-GOES-HERE/ers/config/endpoint/{endpoint_id}"
 
     headers = {
         "Content-Type": "application/json",
